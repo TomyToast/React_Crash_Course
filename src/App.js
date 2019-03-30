a8df5794779e2c62910b92e3796de7877b57767f
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-30 18:41:00 +0100

    set header comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/layout/header';
 import Todos from './components/Todos';
 
 class App extends Component {
@@ -40,6 +41,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
